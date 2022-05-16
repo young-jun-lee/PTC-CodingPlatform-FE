7c5f2c132b375f8a57bd3ae66d2b30ead3d7d082
Youngjun <yjl.2000@hotmail.com>
2022-05-16 11:46:02 -0400

    imported styling and created landing page

---
+++ pages/content/landing.js
@@ -0,0 +1,32 @@
+export default {
+	// About Section --------------
+	aboutParaOne:
+		"PTC is a Canadian nonprofit organization established in May of 2019.",
+	aboutParaTwo:
+		"We're on a mission to inspire and empower the next generation of programmers by providing high school and middle school students with opportunities to discover the world of technology!",
+	//   End About Section ---------------------
+
+	// What Section --------------
+	whatParaOne: `Over FOUR weeks, you'll have THREE weekly multi-part coding problems 
+      of varying difficulty to challenge yourself with! Learning from our tutorials 
+      and post-challenge problem solutions is a great way to improve your problem-solving 
+      and coding skills`,
+	//   End What Section ---------------------
+
+	// Learning Section --------------
+	learningParaOne: `Our goal is to provide free resources to help you learn!`,
+	learningParaTwo: `hints and resources during the contest`,
+	learningParaThree: `post-challenge solutions a week after the deadline closes`,
+	//   End Learning Section ---------------------
+
+	// getStarted Section --------------
+	getStartedParaOne: `Create an `,
+	getStartedParaTwo: `Read the `,
+	getStartedParaThree: `Start Solving weekly `,
+	getStartedParaFour: `Check out your score on your personal leaderboard `,
+	//   End getStarted Section ---------------------
+
+	// ptcMedia Section --------------
+	ptcMediaParaOne: `Follow our socials for updates on PTC events!`,
+	// End ptcMedia Section ---------------------
+};
