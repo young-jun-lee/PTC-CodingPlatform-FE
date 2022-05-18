8944c3d3f9a22f07f10471fa9af238beffab4ca2
seneli <seneli.sene@gmail.com>
2022-05-17 23:53:20 -0400

    imported rules page from 2021 repo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 // import "../styles/globals.css";
-import "../styles/Landing.css";
-import "../styles/Footer.css";
-import "../styles/Banner.css";
-import "../styles/Nav.css";
-import type { AppProps } from "next/app";
-import Footer from "../components/Footer";
+import '../styles/Landing.css';
+import '../styles/Footer.css';
+import '../styles/Banner.css';
+import '../styles/Nav.css';
+import '../styles/RulesBody.css';
+import type { AppProps } from 'next/app';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
