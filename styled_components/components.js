ce58a3d33e4def0aa696506545f5a14fae8e09fa
seneli <seneli.sene@gmail.com>
2022-06-04 15:26:15 -0400

    copied leaderboard page from old project

---
+++ styled_components/components.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Subtitle = styled.h2`
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 29px;
+`;
